f02bfa11575ea22f9ae94ba433b4775e395311df
parvezaman <emailtoamanullah@gmail.com>
2022-02-14 21:05:49 +0600

    basic server started

--- refference/http.js
+++ refference/http.js
@@ -3,6 +3,6 @@ const http = require('http');
 // create a server
 http.createServer((req, res) => {
     // write responses
-    res.write('Hello world');
+    res.write('Hello world. this is the first http server running...');
     res.end();
 }).listen(5000, () => console.log('server is running!'));
